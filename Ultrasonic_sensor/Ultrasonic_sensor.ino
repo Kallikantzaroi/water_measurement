bb46ff0ad09f4a811cb47e76f341166257abe5ac
Julia Liepold <julia.liepold@gmx.at>
2023-02-08 14:48:21 +0100

    update ultrasonic_sensor code to not include library

--- Ultrasonic_sensor/Ultrasonic_sensor.ino
+++ Ultrasonic_sensor/Ultrasonic_sensor.ino
@@ -1,18 +1,33 @@
+#define PIN_TRIGGER 12
+#define PIN_ECHO    11
 
-#include "SR04.h"
-#define TRIG_PIN 12
-#define ECHO_PIN 11
-SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
-long a;
+const int SENSOR_MAX_RANGE = 4000; // in cm
+unsigned long duration;
+unsigned int distance;
 
-void setup() {
-   Serial.begin(9600);
-   delay(1000);
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(PIN_TRIGGER, OUTPUT);
+  pinMode(PIN_ECHO, INPUT);
 }
 
-void loop() {
-   a=sr04.Distance();
-   Serial.print(a);
-   Serial.println("cm");
-   delay(1000);
+void loop()
+{
+  digitalWrite(PIN_TRIGGER, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(PIN_TRIGGER, HIGH);
+  delayMicroseconds(10);
+
+  duration = pulseIn(PIN_ECHO, HIGH);
+  distance = duration/58;
+
+  if (distance > SENSOR_MAX_RANGE || distance <= 0){
+    Serial.println("Out of sensor range!");
+  } else {
+    Serial.println("Distance to water level: " + String(distance) + " cm");
+  }
+
+  delay(1000);
 }
